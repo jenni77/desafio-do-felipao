439c82a4bb6c2ca25bc19dd59a0bb1e2869a6007
jenni77 <jennifersantossilva77@gmail.com>
2024-01-13 13:20:34 -0300

    projetoConcluido

--- index.js
+++ index.js
@@ -3,21 +3,36 @@ const nomeHeroi = ("Nutella")
 let nivelDoHeroi = 10001
 let rankHeroi = ""
 //...
-if(nivelDoHeroi > 0 && nivelDoHeroi <= 1000){
+if(nivelDoHeroi > 0 && nivelDoHeroi <= 1000)
+{
     rankHeroi = ("Ferro.")
-}else if(nivelDoHeroi > 1000 && nivelDoHeroi<=2000){
+}
+else if(nivelDoHeroi > 1000 && nivelDoHeroi<=2000)
+{
     rankHeroi = ("Bronze.")
-}else if(nivelDoHeroi > 2000 && nivelDoHeroi<=5000){
+}
+else if(nivelDoHeroi > 2000 && nivelDoHeroi<=5000)
+{
     rankHeroi = ("Prata.")
-}else if(nivelDoHeroi > 5000 && nivelDoHeroi<=7000){
+}
+else if(nivelDoHeroi > 5000 && nivelDoHeroi<=7000)
+{
     rankHeroi = ("Ouro.")
-}else if(nivelDoHeroi > 7000 && nivelDoHeroi<=8000){
+}
+else if(nivelDoHeroi > 7000 && nivelDoHeroi<=8000)
+{
     rankHeroi = ("Platina.")
-}else if(nivelDoHeroi > 8000 && nivelDoHeroi<=9000){
+}
+else if(nivelDoHeroi > 8000 && nivelDoHeroi<=9000)
+{
     rankHeroi = ("Ascendente.")
-}else if(nivelDoHeroi > 9000 && nivelDoHeroi<=10000){
+}
+else if(nivelDoHeroi > 9000 && nivelDoHeroi<=10000)
+{
     rankHeroi = ("Imortal.")
-}else if(nivelDoHeroi> 10000){
+}
+else if(nivelDoHeroi> 10000)
+{
     rankHeroi = ("Radiante")
 }
 //console
